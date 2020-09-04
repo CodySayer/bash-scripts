27d3e188c8778f038adfc8a4ec0210be614d2ebf
Cody Sayer <codymathewsayer@gmail.com>
2020-09-04 11:51:14 -0700

    initial commit

---
+++ ubuntu-update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+BLUE='\033[0;36m'
+GREEN='\e[32m'
+NC='\033[0m'
+set -m
+
+echo
+echo -e [${BLUE}STARTING UPDATES{NC}]
+    echo
+    echo -e --${BLUE}updating APT repos${NC}
+    sleep 2
+    echo
+    sudo apt-get update
+    sleep 2
+    echo
+    echo -e --${GREEN}repo update complete${NC}
+    echo
+    echo -e --${BLUE}updating APT packages${NC}
+    sleep 2
+    echo
+    sudo apt-get upgrade
+    sleep 2
+    echo
+    echo -e --${GREEN}package update complete${NC}
+    echo
+    echo -e --${BLUE}updating SNAP packages${NC}
+    sleep 2
+    echo
+    sudo snap refresh
+    sleep 2
+    echo
+    echo -e --${GREEN}SNAP update complete${NC}
